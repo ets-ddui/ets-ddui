5e730841f5f53f3fc5395b27c661ccd4bcc87c1d
Steven <xinghun87@163.com>
2022-04-01 20:44:59 +0800

    目录结构调整

--- build.bat
+++ build.bat
@@ -2,9 +2,9 @@ set path=%path%;C:\Windows\Microsoft.NET\Framework\v2.0.50727
 set BDS=C:\Program Files (x86)\CodeGear\RAD Studio\5.0
 set BinDir=%~dp0Out
 
-MSBuild.exe Delphi\DDUI\dclDDUI.dproj
-MSBuild.exe Delphi\DDUI\DDUI.dproj
-::MSBuild.exe Delphi\ThirdParty\JCL\JCL.dproj
-MSBuild.exe Delphi\Demo\ทย360\Fake360.dproj
+MSBuild.exe DDUI\dclDDUI.dproj
+MSBuild.exe DDUI\DDUI.dproj
+::MSBuild.exe ThirdParty\JCL\JCL.dproj
+MSBuild.exe Demo\ทย360\Fake360.dproj
 
 pause
